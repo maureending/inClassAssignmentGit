07944ca4ba9b62b67bd3933eff7337818dc4c1a0
Maureen_Ding <md3474@tc.columbia.edu>
2017-03-04 23:00:17 -0500

    Clear the arraylist every time the user choose to show the result

--- app/src/main/java/jiajing/inclassassignment05_jiajingc/MainActivity.java
+++ app/src/main/java/jiajing/inclassassignment05_jiajingc/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra("studentList", studentList);
+        studentList = new ArrayList<>();
         //studentList = new ArrayList<>();
         startActivity(intent);
     }
